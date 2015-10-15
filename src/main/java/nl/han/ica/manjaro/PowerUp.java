5159435ea6b0b0e27794f5da9d9a4ecf8969892c
Jimmy-F <feltsadas.j@gmail.com>
2015-10-15 10:55:10 +0200

    powerup

--- src/main/java/nl/han/ica/manjaro/PowerUp.java
+++ src/main/java/nl/han/ica/manjaro/PowerUp.java
@@ -1,24 +1,49 @@
 package nl.han.ica.manjaro;
+import java.util.Timer;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Alarm.Alarm;
+import nl.han.ica.OOPDProcessingEngineHAN.Alarm.IAlarmListener;
 
-public class PowerUp {
+public abstract class PowerUp implements IAlarmListener {
 
 	private int cooldown;
-
-	private int speedValue;
+	
+	private boolean ready;
 
 	private Manjaro manjaro;
 
-	public PowerUp(Manjaro game) {
-		
+
+	public PowerUp(Manjaro game, int cooldown) {
+		this.manjaro = game;
+		this.cooldown = cooldown;
+		ready = false;
 	}
 
 	public void activate() {
-
+		if(!ready) {
+			return;
+		}
+		
+		startAlarm(); // reset the cooldown
 	}
 
 	public void update() {
 
 	}
+	
+	 private void startAlarm() {
+	    Alarm alarm=new Alarm(null, cooldown);
+	    alarm.addTarget(this);
+	    alarm.start();
+	    ready= false;
+	}
 
+	@Override
+	public void triggerAlarm(String alarmName) {
+		ready = true;
+	}
+	
+	public boolean isReady() {
+		return ready;
+	}
 }
